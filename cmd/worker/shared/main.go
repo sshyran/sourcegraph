f6c45d2129535baebf5d8638c1ebaf4d81399958
Adam Harvey <adam@adamharvey.name>
2022-12-28 14:42:19 -0800

    Add untested outbound webhook worker.

--- cmd/worker/shared/main.go
+++ cmd/worker/shared/main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/worker/internal/encryption"
 	"github.com/sourcegraph/sourcegraph/cmd/worker/internal/gitserver"
 	workermigrations "github.com/sourcegraph/sourcegraph/cmd/worker/internal/migrations"
+	"github.com/sourcegraph/sourcegraph/cmd/worker/internal/outboundwebhooks"
 	"github.com/sourcegraph/sourcegraph/cmd/worker/internal/repostatistics"
 	"github.com/sourcegraph/sourcegraph/cmd/worker/internal/webhooks"
 	"github.com/sourcegraph/sourcegraph/cmd/worker/internal/zoektrepos"
@@ -49,6 +50,7 @@ func Start(observationCtx *observation.Context, additionalJobs map[string]job.Jo
 		"record-encrypter":          encryption.NewRecordEncrypterJob(),
 		"repo-statistics-compactor": repostatistics.NewCompactor(),
 		"zoekt-repos-updater":       zoektrepos.NewUpdater(),
+		"outbound-webhook-sender":   outboundwebhooks.NewSender(),
 	}
 
 	jobs := map[string]job.Job{}
