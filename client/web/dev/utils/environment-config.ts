cdb76adfc9faffbb7fd57e69acbcf28688669cbe
Quinn Slack <quinn@slack.org>
2023-01-02 21:13:35 -1000

    make esbuild the default builder for the web app
    This is essentially blocked on https://github.com/evanw/esbuild/pull/1458 because:
    
    - That PR is needed to make tree-shaking work with esbuild in our app.
    - We could just use esbuild for dev (where tree-shaking isn't as necessary) and Webpack in prod, but we don't want to use a different builder in dev vs. prod.
    - Using an esbuild fork is cumbersome, since we'd also need to build the `esbuild` binaries for various platforms.

--- client/web/dev/utils/environment-config.ts
+++ client/web/dev/utils/environment-config.ts
@@ -55,9 +55,10 @@ export const ENVIRONMENT_CONFIG = {
     // Sentry project
     SENTRY_PROJECT: process.env.SENTRY_PROJECT,
 
-    //  Webpack is the default web build tool, and esbuild is an experimental option (see
-    //  https://docs.sourcegraph.com/dev/background-information/web/build#esbuild).
-    DEV_WEB_BUILDER: (process.env.DEV_WEB_BUILDER === 'esbuild' ? 'esbuild' : 'webpack') as WEB_BUILDER,
+    // esbuild is the default web build tool
+    // (https://docs.sourcegraph.com/dev/background-information/web/build#esbuild). Webpack is
+    // deprecated and will be removed soon.
+    DEV_WEB_BUILDER: (process.env.DEV_WEB_BUILDER === 'webpack' ? 'webpack' : 'esbuild') as WEB_BUILDER,
 
     /**
      * Omit slow deps (such as Monaco and GraphiQL) in the build to get a ~40% reduction in esbuild
